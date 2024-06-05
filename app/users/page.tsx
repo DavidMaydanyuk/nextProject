4d5afb3a24dc8daa1c8340ebebe4d1fd3e1bbbdb
DavidMaydanyuk <davidmaydan@gmail.com>
2024-06-05 11:17:58 +0200

    caching

---
+++ app/users/page.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+interface User {
+  id: string;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: { revalidate: 10 }  // next will refresh cache every 10 seconds
+    // { cache: 'no-store' } to disable caching: for data that changes frequently 
+  });
+  // sending http request to backend
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users:</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default UsersPage;
